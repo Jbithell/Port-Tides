0dbe3b9cc5cf0f130f98ba119dcb6c3e270fb1ab
James Bithell <Jbithell@users.noreply.github.com>
2021-07-23 17:01:04 +0100

    Create a PWA (#2)
    * Create a PWA
    
    * Make icon maskable

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,6 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
@@ -29,6 +23,30 @@ module.exports = {
         langKeyForNull: "en",
         prefixDefault: false,
         useLangKeyLayout: false,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Porthmadog Tide Times",
+        short_name: "Port-Tides",
+        description: "Free tidal predictions using data from the UK Hydrographic Office for the town of Porthmadog, and associated villages",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#93c5fd",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        icon_options: {
+          purpose: "any maskable",
+        },
+        cache_busting_mode: "none",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        globPatterns: ["**/images/*"],
+        precachePages: ["**/tide-tables/*"],
       },
     },
   ],
