62b5aa08e2caafdcaf58d6c49d617e640f99af8d
James Bithell <Jbithell@users.noreply.github.com>
2023-04-21 18:08:56 +0100

    Add copyright year

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 
 const Copyright = (props) => {
@@ -5,10 +6,18 @@ const Copyright = (props) => {
     <>
       <div className="p-4 bg-white bg-opacity-95">
         <p>{props.lang == "cy" ? "Yr amseroedd yw GMT/BST." : "Times are GMT/BST."} Tidal Predictions are provided for use by all water users though the developers of this site can not be held accountable for the accuracy of this data or any accidents that result from the use of this data.<br/>
-          All Tidal Data is ©Crown Copyright. Reproduced by permission of the Controller of Her Majesty's Stationery Office and the UK Hydrographic Office (www.ukho.gov.uk). No tidal data may be reproduced without the expressed permission of the UKHO licencing department. {props.lang == "cy" ? "Gwefan, PDF ac App" : "PDFs, Website & App"} ©2014-2021 James Bithell</p>
+          All Tidal Data is ©Crown Copyright. Reproduced by permission of the Controller of Her Majesty's Stationery Office and the UK Hydrographic Office (www.ukho.gov.uk). No tidal data may be reproduced without the expressed permission of the UKHO licencing department. {props.lang == "cy" ? "Gwefan, PDF ac App" : "PDFs, Website & App"} ©2014-{buildYear} James Bithell</p>
       </div>
     </>
   );
 };
+
+const buildYear = useStaticQuery(graphql`
+  query {
+    currentBuildDate {
+      currentDate
+    }
+  }
+`);
 
 export default Copyright;
