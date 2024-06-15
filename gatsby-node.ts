73dc64bd36d4f262a9d757665c80370be5445448
James Bithell <Jbithell@users.noreply.github.com>
2024-06-15 13:08:17 +0100

    Add iCal file

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,10 @@
 import TidalData from "./data/tides.json";
 import path from "path";
-import { TidesJson_PDFObject } from "./src/types";
-import { CreatePagesArgs } from "gatsby";
+import { TidesJson_PDFObject, TidesJson_ScheduleObject } from "./src/types";
+import { BuildArgs, CreatePagesArgs } from "gatsby";
+import fs from "fs";
+import ical, { ICalEventBusyStatus, ICalEventClass } from "ical-generator";
+import { DateTime } from "luxon";
 
 export const createPages = async function ({
   actions,
@@ -29,4 +32,40 @@ export const createPages = async function ({
     fromPath: "/historical-tables",
     toPath: "/tide-tables",
   });
+};
+
+export const onPostBootstrap = function ({ reporter }: BuildArgs) {
+  reporter.info(`Generating iCal file for tide times`);
+  const cal = ical();
+  cal.timezone("Europe/London");
+  cal.name("Porthmadog Tide Times");
+  cal.description(
+    "Tide times for Porthmadog, Borth-y-gest, Morfa Bychan and Black Rock Sands from Port-Tides.com"
+  );
+  TidalData.schedule.forEach((day: TidesJson_ScheduleObject) =>
+    day.groups.forEach((tide) => {
+      cal.createEvent({
+        start: DateTime.fromSQL(day.date + " " + tide.time).toJSDate(),
+        end: DateTime.fromSQL(day.date + " " + tide.time)
+          .plus({ minutes: 30 })
+          .toJSDate(),
+        summary: `High Tide - ${tide.height}m`,
+        busystatus: ICalEventBusyStatus.FREE,
+        class: ICalEventClass.PUBLIC,
+        description: {
+          plain: `High Tide ${tide.height}m at ${tide.time}`,
+          html: `High Tide ${tide.height}m at ${tide.time}. More details at <a href="https://port-tides.com/">port-tides.com</a>`,
+        },
+        location: {
+          title: "Porthmadog",
+          address: "Harbwr Porthmadog, LL49 9AY, UK",
+        },
+        url: "https://port-tides.com/tide-tables",
+      });
+    })
+  );
+  fs.writeFileSync("public/porthmadog-tides.ical", cal.toString());
+  reporter.success(
+    `Generated iCal file for tide times at public/porthmadog-tides.ical`
+  );
 };
