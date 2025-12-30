76019f203ac4fee8ef5abf9cb0b6f3f49b31001c
James Bithell <Jbithell@users.noreply.github.com>
2025-12-30 17:05:06 +0000

    Fix pdfs not showing

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ const config = defineConfig({
         enabled: true,
         crawlLinks: true, // Render dynamic routes like the tide table pages because they are linked to from pages
         autoStaticPathsDiscovery: true,
-        concurrency: 7, // Cloudflare workers build runs out of memory quite easily
-        filter: ({ path }) => path !== "/" // Do not prerender the home page
+        concurrency: 14, // Cloudflare workers build runs out of memory quite easily
+        filter: ({ path }) => path !== "/" && !path.endsWith(".pdf") // Do not prerender the home page or PDFs, as these get copied from the public directory
       },
       sitemap: {
         enabled: true,
